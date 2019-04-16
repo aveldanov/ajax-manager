fd31d3d7cb526c88ccf8e2cc9b3f7482a99c7532
Anton Veldanov <anton.veldanov@gmail.com>
2019-04-15 19:48:07 -0700

    first commit

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                this.setState({
+                    posts: res.data
+                })
+                console.log(res.data);
+
+            })
+    }
     render() {
+        const posts = this.state.posts.map((post) => {
+            console.log(post);
+
+            return <Post key={post.id} title={post.title} />;
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
